44951cb19897fcf55ae43ade1d7bee942136e88d
MOstrenina <MOstrenina@luxoft.com>
2020-02-11 17:39:37 +0300

    Add new method register_new_user(email, password) for LoginPage.

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,6 +3,11 @@ from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+    def __init__(self, *args, email=None, password=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.email = email
+        self.password = password
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
@@ -21,4 +26,10 @@ class LoginPage(BasePage):
         # проверка наличия формы регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not found!"
 
+    def register_new_user(self):
+        self.browser.find_element(*LoginPageLocators.REGISTER_EMAIL).send_keys(self.email)
+        self.browser.find_element(*LoginPageLocators.REGISTER_FIRST_PASSWORD).send_keys(self.password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_CONFIRM_PASSWORD).send_keys(self.password)
+        submit_button = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
+        submit_button.click()
 
