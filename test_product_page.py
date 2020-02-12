f2a4cda7be9a55234f235ebc6fcbf3dcad4fc1fd
MOstrenina <MOstrenina@luxoft.com>
2020-02-12 16:34:14 +0300

    Remove redundant imports from test_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -3,9 +3,6 @@ from pages.basket_page import BasketPage
 from pages.login_page import LoginPage
 from pages.main_page import MainPage
 import pytest
-import time
-from time import sleep
-
 from mimesis import Person  # с помощью этой библиотеки будем генерировать email и password
 
 
@@ -20,7 +17,6 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.should_be_product_price()
     page.should_be_add_product_to_basket()
     page.solve_quiz_and_get_code()
-    # time.sleep(2)
     page.product_should_be_added_to_basket()
     page.item_price_should_be_the_same()
     page.product_should_have_the_same_name()
@@ -59,12 +55,14 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+
 @pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
 
 @pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
@@ -114,6 +112,4 @@ class TestUserAddToBasketFromProductPage:
         page.should_be_add_to_basket_button()
         page.should_be_add_product_to_basket()
         page.should_not_be_success_message()
-
-
 
